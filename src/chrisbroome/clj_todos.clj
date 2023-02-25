eeefa4418f74dc59cd60dceee727ffeb63a53cd7
Chris Broome <chrisbroome7@gmail.com>
2023-02-25 10:01:25 -0500

    Set up basic compojure handlers

--- src/chrisbroome/clj_todos.clj
+++ src/chrisbroome/clj_todos.clj
@@ -1,5 +1,25 @@
 (ns chrisbroome.clj-todos
+  (:require [compojure.core :refer [GET defroutes]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.defaults :refer [site-defaults
+                                              wrap-defaults]]
+            [ring.util.response :as resp]
+            [selmer.parser :as html])
   (:gen-class))
+
+(defn handler [req]
+  (-> (resp/response (html/render-file "index.html" {:name "the repl"}))
+      (doto tap>)
+      (resp/content-type "text/html")))
+
+(defn favicon [req]
+  (resp/resource-response "images/favicon.ico"))
+
+(defroutes c-router
+  (GET "/favicon.ico" [] #'favicon)
+  (GET "/" [] #'handler))
+
+(def app (wrap-defaults #'c-router site-defaults))
 
 (defn greet
   "Callable entry point to the application."
